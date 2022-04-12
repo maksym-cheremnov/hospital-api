454d71a9f1372a4eab028379e6d9177023a52096
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2022-04-12 18:34:36 +0300

    Add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Odecut135924@',
-      database: 'app',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [],
       synchronize: true,
     }),
