0c1b96b948c23a5f0703c057f09728c756e1716a
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2022-04-12 18:27:05 +0300

    Generate services and additional modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientModule } from './patient/patient.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [],
+  imports: [PatientModule, AppointmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
