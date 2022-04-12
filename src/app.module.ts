f6fdfddf47dcbeedc242fa61becb7bbe07f1906e
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2022-04-12 18:31:50 +0300

    Add .env with example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
 import { AppointmentModule } from './appointment/appointment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PatientModule, AppointmentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Odecut135924@',
+      database: 'app',
+      entities: [],
+      synchronize: true,
+    }),
+    PatientModule,
+    AppointmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
